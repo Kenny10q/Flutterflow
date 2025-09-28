ee081d322ab3a93a19b54138b03de32f9e96729c
Kenny_10q <kenny26757@gmail.com>
2025-09-28 17:15:24 -0400

    Primer commit - subiendo proyecto Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,21 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  constructor(private platform: Platform, private iab: InAppBrowser) {}
 
-  constructor() {}
-
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      if (this.platform.is('cordova') || this.platform.is('capacitor')) {
+        // Carga web en InAppBrowser en móvil
+        this.iab.create(
+          'https://www.flutterflow.io/',
+          '_self',  // abre en la misma ventana
+          'location=no,zoom=no,hardwareback=yes'
+        );
+      } else {
+        // Fallback en navegador
+        window.location.href = 'https://www.flutterflow.io/';
+      }
+    });
+  }
 }
