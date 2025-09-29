3ce2109316e03d45db6506f0e6837150560aaadc
Kenny_10q <kenny26757@gmail.com>
2025-09-28 20:00:26 -0400

    ,subuendo

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,21 +1,15 @@
 package io.ionic.starter;
 
 import android.os.Bundle;
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.BridgeWebView;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Obtén el WebView de Capacitor
-        BridgeWebView webView = (BridgeWebView) this.getBridge().getWebView();
-
-        // Cambiar user-agent para forzar versión de escritorio
-        webView.getSettings().setUserAgentString(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        this.getBridge().getWebView().getSettings().setUserAgentString(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0 Safari/537.36"
         );
     }
 }
